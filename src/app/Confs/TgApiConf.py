d8f4e059b6d9a6936e31856a63ec7680fa3fb0e7
RikiSem <112815362+RikiSem@users.noreply.github.com>
2024-11-27 15:11:39 +0300

    fix

--- src/app/Confs/TgApiConf.py
+++ src/app/Confs/TgApiConf.py
@@ -1,7 +1,7 @@
-logChannel = -1002398131549
-photoChannel = '@RandomFotoChannelTest'
-videoCannel = '@RandomVideoChannelTest'
+logChannel = -
+photoChannel = '@'
+videoCannel = '@'
 
 class TgApiConf:
-    token = "7644434924:AAGjtIPQFhkE_DbCSXw4HljnJ08eDMjmf5Y"
+    token = ""
     https = "https://api.telegram.org/bot{0}".format(token)
