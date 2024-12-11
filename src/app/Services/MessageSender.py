0ede7ca8325a01fafd0bd6c6f0d3de18fb9a873b
RikiSem <evaskes@team.amocrm.com>
2024-12-11 12:09:01 +0300

    fix

--- src/app/Services/MessageSender.py
+++ src/app/Services/MessageSender.py
@@ -10,8 +10,10 @@ class MessageSender(BaseService):
         await self.logger.writeLog('Началась рассылка сообщения')
         for user in users:
             userId = user[self.userRepository.field_id]
-            await self.bot.send_message(
-                chat_id=userId,
-                text=text
-            )
+            #await self.bot.send_message(
+            #    chat_id=userId,
+            #    text=text
+            #)
+            print(userId)
+            await self.logger.writeLog(F'Сообщение отправлено юзеру {userId}')
         await self.logger.writeLog('Рассылка сообщения закончилась')
